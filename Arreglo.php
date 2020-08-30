7dbf2dac897b9ee9370f0213cfbac59039a1979b
Noé Muñoz-Pérez <sightceo@gmail.com>
2020-08-30 12:59:26 -0500

    Update Arreglo.php

--- Arreglo.php
+++ Arreglo.php
@@ -1,10 +1,22 @@
 <?php
 class Arreglo
 {
- private $Arreglo[];
- public __function ordena();
- public __function cuenta();
- public __function medVar();
- public __function mostrar();
+ private $Arreglo;
+ public function __constructor()
+ {
+  $this->Arreglo = array();
+ }
+ public function __constructor($n)
+ {
+  $this->Arreglo = array();
+  for($i = 0; $i < $n; $i++)
+  {
+   $this->Array[i] = rand();
+  }
+ }
+ public function ordena();
+ public function cuenta();
+ public function medVar();
+ public function mostrar();
 }
 ?>
