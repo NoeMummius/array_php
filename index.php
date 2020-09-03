71026bd674b195ffe381942551185487ed394290
Noé Muñoz-Pérez <sightceo@gmail.com>
2020-09-03 00:14:39 -0500

    Update index.php
    Implementación de evidencias, y funciones completas a excepción del llenado del arreglo con números aleatorios

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
    <h1>Aplicaciones Web</h1>
    <div class="Resumen">
     <ol>
-     <li>Cuamatzi Flores, Juan</li>
+     <!--li>Cuamatzi Flores, Juan</li-->
      <li>Muñoz Pérez, Noé</li>
-     <li>Rojas Cano, Adriana Abigail</li>
-     <li>Trujillo Carrera, Arturo</li>
+     <li>Rojas Cano, Ariadna Abigail</li>
+     <!--li>Trujillo Carrera, Arturo</li-->
     </ol>
     <table border="1">
      <tr>
@@ -22,34 +22,48 @@
       <td>Fecha</td>
      </tr>
      <tr>
-      <td>Actividad #</td>
-      <td>Título de la actividad</td>
-      <td>Descripción.</td>
-      <td><img src="evidencias/slack_actividad_4_1.png">
-      <br><img src="evidencias/slack_actividad_4_2.png">
-      <br><img src="evidencias/github_actividad_4_1.png">
-      <br><img src="evidencias/github_actividad_4_2.png"></td>
+      <td>Actividad 5</td>
+      <td>Arreglos en PHP Orientado a Objetos</td>
+      <td>En ésta actividad se desarrolló un script en PHP utilizando el paradigma orientado a objetos para realizar algunas operaciones básicas sobre arreglos. Las operaciones incluídas en el script son:<br>
+      <ul>
+          <li>Mostrar</li>
+          <li>Ordenar ascendentemente</li>
+          <li>Calcular media y varianza de los datos</li>
+          <li>Llenar con números introducidos por el usuario</li>
+          <li>Llenar con números aleatorios</li>
+      </ul></td>
+      <td><img src="evidencias/actividad_5_slack_1.png">
+      <br><img src="evidencias/actividad_5_slack_2.png">
+      <br><a href="https://github.com/NoeMummius/array_php">Github del proyecto por <i>Noé Muñoz</i></a>
+      <br>
+      <?php
+       $fp = fopen("evidencias/contar.txt","r");
+       while(!feof($fp))
+       {
+        $linea=fgets($fp);
+        echo $linea.'<br>';
+       }
+      ?>
+      <label>Código para ordenar el arreglo escrito por <i>Ariadna Rojas</i></label></td>
       <td>Tue. Sep. 1, 2020 10:54</td>
      </tr>
     </table>
    </div>
+   <br>
    <div class="Actividad">
     <form action="index.php" method="POST">
-     <label for="crear">Escriba los enteros que llevará el arreglo separados por comas: </label>
+     <label for="crear">Escriba los números que llevará el arreglo separados por comas: </label>
      <input type="text" name="data">
-     <br>
      <input type="submit" value="Enviar datos">
      <br>
-     <label for="llenar">Presione llenar para crear un arreglo con enteros aleatorios</label>
-     <input type="submit" value="Enviar random">
+     <label for="llenar">Presione llenar para crear un arreglo con números aleatorios</label>
+     <input type="submit" value="Llenar aleatoriamente">
      <br>
      <label for="orden">Elija cómo desea ver los datos:</label>
      <select name="orden" id="orden">
        <option value="D">Desordenados</option>
        <option value="O">Ordenados</option>
      </select>
-     <br><br>
-     <input type="submit" value="Enviar orden">
     </form>
     <?PHP
      require("arreglo.php");
@@ -57,12 +71,17 @@
      if (isset($_POST['data']))
      {
       $Numeros = explode(",", $_REQUEST['data']);
-      $Enteros = new Arreglo($Numeros);
+      $Enteros = new Arreglo();
+      foreach($Numeros as $numero)
+      {
+       $Enteros->add($numero);
+      }
      }
-     else if (isset($_POST['Enviar random']))
+     else if(isset($_REQUEST['Llenar aleatoriamente']))
      {
-      $n = rand();
-      $Enteros = new Arreglo($n);
+      $Enteros = new Arreglo();
+      $Enteros->auto_fill();
+      $Enteros->contara();
      }
      if (isset($_POST['orden']))
      {
@@ -73,11 +92,14 @@
          $Enteros->mostrar()."<br>"; 
         break;
         case 'O':
-         $CopyEnteros = $Enteros;
-         $CopyEnteros->ordenar();
-         $CopyEnteros->mostrar();
+         $Enteros->ordenar();
+         $Enteros->mostrar().'<br>';
         break;
       }
+      $med = 0;
+      $var = 0;
+      [$med, $var] = $Enteros->med_var();
+      echo 'Media: '.$med.'<br>Varianza: '.$var.'<br>';
      }
     ?>
    </div>
